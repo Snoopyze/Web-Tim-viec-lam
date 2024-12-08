54a198f634d0e5ccad4438d5876c0e41f65bb575
dhmh204 <daohoangminhhang24@gmail.com>
2024-12-08 09:04:55 +0700

    update

---
+++ FrontEnd/Controllers/DanhSachCongTy.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class DanhSachCongTy : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
